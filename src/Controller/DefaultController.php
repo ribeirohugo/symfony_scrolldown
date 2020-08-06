bda73322e441eea8a0703e1c913990444599dd9d
Hugo Ribeiro <ribeirohugo.op@gmail.com>
2020-08-06 18:00:03 +0000

    #4 Property empty homepage for testing.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -27,4 +27,48 @@ class DefaultController extends AbstractController {
         ]);
     }
 
+    /**
+     * @Route("/empty", name="home_empty")
+     */
+    public function indexEmpty(Request $request)
+    {
+		//Get Json file
+		$content = file_get_contents('../properties-2.json');
+
+		//Decode Json file into array
+		$json = json_decode($content, true);
+
+        return $this->render('default/default.html.twig', [
+			'title' => 'Hotels',
+			'content' => $json,
+			'properties' => []
+        ]);
+    }
+
+
+    /**
+     * @Route("/import", name="webhook_emailbidding")
+     */
+    public function weebhookEmailBidding(Request $request, Connection $con, TranslatorInterface $translator)
+	{
+		if ('json' === $request->getContentType()) {
+
+			$encoders = [new JsonEncoder()];
+			$normalizers = [new ObjectNormalizer()];
+
+			$data = $request->getContent();
+			$serializer = new Serializer($normalizers, $encoders);
+
+			$webhook = $serializer->deserialize($data, Webhook::class, 'json');
+
+			$entityManager = $this->getDoctrine()->getEntityManager();
+			$entityManager->persist($webhook);
+			$entityManager->flush();
+
+			return new JsonResponse('sucesso');
+		} else {
+
+			return new JsonResponse($translator->trans('Erro'));
+		}
+	}
 }
